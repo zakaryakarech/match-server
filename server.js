ac2daecc2995c0b2fa09b32fcc3ec65d6295d65a
zakaryakarech <137610424+zakaryakarech@users.noreply.github.com>
2026-06-13 10:06:05 +0100

    Change service account loading method for Firebase
    Update Firebase Admin initialization to use environment variable for service account JSON.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // ========== إعداد Firebase Admin ==========
 // ضع ملف serviceAccountKey.json في نفس المجلد
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_JSON);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
